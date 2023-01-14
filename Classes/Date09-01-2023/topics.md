5972c8f55fee9eb7a154070414267711226bb7be
harishkumarforever <harishking809@gmail.com>
2023-01-14 07:54:02 +0530

    small changes added

--- Classes/Date09-01-2023/topics.md
+++ Classes/Date09-01-2023/topics.md
@@ -5,4 +5,16 @@ margin,
 padding,
 position,
 flex,
-grid,media,screen ,css box model,zindex,negative margin,important,id ,class,border,width,height ,overflow,alignment ,min width,max width, before after pseudo classes and hover, focus,inline and inline,block
+grid , 
+text-align,
+css box model,
+zindex,
+negative margin,
+important,
+border,width,height ,
+overflow,alignment ,
+min width,max width,
+before after pseudo classes
+and hover, focus,
+inline and
+block inline,block,,media,screen
--- src/App.js
+++ src/App.js
@@ -7,14 +7,16 @@ import whatapp from "./WhatsApp Image 2023-01-05 at 2.09.21 PM.jpeg";
 
 const App = () => {
   return (
-    <div>
-    <h1  >sssssssssssssss</h1> 
-    <p id="hshhshshhs">im a p tag</p>
-    <p  className="myhadung">im a p tag</p>
-    
-    <h1  >sssssssssssssa</h1> 
-    <h1  >wwwww</h1> 
-    <p className="myhadung">im a p tag</p>
+    <div className="container">
+    <h1 className="first">sssssssssssssss </h1>
+      <h1 className="first">111111111 </h1>
+      <h1 className="first">23333333 </h1>
+      <h1 className="first">333 </h1>
+      <h1 className="first">dd </h1>
+      <h1 className="first">ddd </h1>
+      <h1 className="first">da </h1>
+      <h1 className="first">sssssssssssssss </h1>
+      
     </div>
   );
 };
