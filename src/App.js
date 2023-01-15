bcace17ec54a897319bd168df49836b3e774c0c2
harishkumarforever <harishking809@gmail.com>
2023-01-15 13:29:18 +0530

    small changes added

--- src/App.js
+++ src/App.js
@@ -3,42 +3,9 @@ import "./App.scss";
 
 const App = () => {
   return (
-    <div className="container">
-      {/* <div className="myborder">1234</div> */}
-      {/* <div className="overflowTest">
-        This text is really long and the height of its container is only 100
-        pixels. Therefore, a scrollbar is added to help the reader to scroll the
-        content. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed
-        diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
-        volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
-        ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
-        Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse
-        molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero
-        eros et accumsan et iusto odio dignissim qui blandit praesent luptatum
-        zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber
-        tempor cum soluta nobis eleifend option congue nihil imperdiet doming id
-        quod mazim placerat facer possim assum. Typi non habent claritatem
-        insitam; est usus legentis in iis qui facit eorum claritatem.
-      </div> */}
-      {/* <div className="vsgvsg">center me</div> */}
-      {/* <div className="minWwwww">123</div> */}
-      {/* <div className="maxWwwww">
-        123
-      </div> */}
-      {/* <input className="myInput" /> */}
-
-      {/* <div className="h1tags">
-        <h1>one</h1>
-        <h1>two</h1>
-        <h1>tree</h1>
-        <h1>four</h1>
-        <h1>five</h1>
-      </div> */}
-
-      <div className="medidiidid">
-        <h1>im ok <span>dddddddddddddd</span></h1>  
-      </div>
-    </div>
+     <div>
+      
+     </div>
   );
 };
 export default App;
--- src/components/Antd.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Button, Space } from "antd";
-const ANTD = () => (
-  <Space wrap>
-    <Button type="primary">Primary Button</Button>
-  </Space>
-);
-export default ANTD;
